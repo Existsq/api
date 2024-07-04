a6b9d8fada2c738885ba9d71cff2bca9635ce9b0
Exist <existqq@gmail.com>
2024-07-04 19:00:01 +0300

    Updated front-end

--- next-layer/app/page.tsx
+++ next-layer/app/page.tsx
@@ -4,18 +4,28 @@ import Image from "next/image";
 import LoginForm from "@/components/LoginForm";
 import SignUpForm from "@/components/SignUpForm";
 import Header from "@/components/Header";
-
+import Search from "@/components/Search";
+import Categories from "@/components/Categories";
+import ProductCard from "@/components/ProductCard";
 
 const poppins = Barlow({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-
 export default function Home() {
   return (
-    <main className="">
-        <Header></Header>
+    <main className="bg-white min-h-screen ">
+      <Header></Header>
+      <div className="flex py-5 justify-center align-middle mx-5">
+        <Search></Search>
+      </div>
+      <div className="py-5 align-middle mx-5">
+        <Categories></Categories>
+      </div>
+      <div className="py-5 align-middle mx-5">
+        <ProductCard></ProductCard>
+      </div>
     </main>
   );
 }
--- next-layer/components/Header.tsx
+++ next-layer/components/Header.tsx
@@ -2,21 +2,19 @@ import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoNotificationsOutline } from "react-icons/io5";
 import { IoCartOutline } from "react-icons/io5";
-import { IconContext } from "react-icons";
-
 
 function Header() {
   return (
-    <div className="mx-4 py-4 bg-white text-black">
-      <div className="flex flex-row justify-around align-middle">
+    <div className="justify-center shadow-lg align-center px-5 py-4 bg-white text-black sticky">
+      <div className="flex flex-row justify-between align-middle gap-3">
           <div className="flex justify-center align-middle">
-            <GiHamburgerMenu />
+            <GiHamburgerMenu size="25" className="align-middle justify-center"/>
           </div>
-        <div className="flex flex-row justify-around align-middle w-full">
-          <p>Home</p>
-          <div className="flex flex-row justify-center align-middle w-fit">
-            <IoNotificationsOutline />
-            <IoCartOutline />
+        <div className="flex flex-row justify-between align-middle w-full">
+          <p className="text-2xl font-medium">Home</p>
+          <div className="flex flex-row justify-center align-middle v-al w-fit gap-3">
+            <IoNotificationsOutline size="25" className="flex justify-center align-middle"/>
+            <IoCartOutline size="25" className="align-middle"/>
           </div>
         </div>
       </div>
